ffb098c7d0b3bc1c1811c618af00240fe3a03fc5
Drillgon200 <Drillgon200@gmail.com>
2019-10-09 11:30:12 -0700

    Removed all java files because I'll have to start over.

--- com/hbm/tileentity/conductor/TileEntityCableSwitch.java
+++
@@ -1,9 +0,0 @@
-package com.hbm.tileentity.conductor;
-
-import com.hbm.interfaces.IConductor;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityCableSwitch extends TileEntityWireCoated {
-
-}
