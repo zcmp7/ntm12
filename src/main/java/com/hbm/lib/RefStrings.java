3e76b13ec642b26dbd5d42a1240a7eecfeeb2b20
Alcater <alcater.fallout4@gmail.com>
2022-12-31 18:46:19 +0100

    Ups

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -4,7 +4,7 @@ public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Hbm's Nuclear Tech - Alcater Edition";
 	public static final String VERSION = "hbm-A-1.12.2-1.8.4c";
-	public static final String CHANGELOG = "§2Added two new Meteorite Swords$§2Added JEI DFC support$§3Changed Meteorite Sword Guide Book$§3Changed Unobtainium Rod Balance$§6Fixed RBMK Rod Fuction Display$§6Fixed Hadron Coil Recipe Loop";
+	public static final String CHANGELOG = "§2Added two new Meteorite Swords$§2Added JEI DFC support$§3Changed Meteorite Sword Guide Book$§3Changed Unobtainium Rod Balance$§6Fixed RBMK Rod Function Display$§6Fixed Hadron Coil Recipe Loop";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
